7c47871187e118535951e4a11038b16086d28ad8
Ayman Salah <80077545+AymanSalah1994@users.noreply.github.com>
2022-09-08 07:22:53 +0200

    Debugged Some UI problems

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,12 +1,14 @@
 @extends('layouts.dashboard.main_panel')
 @section('content')
-    
     <div class="card">
         <div class="card-body">
-           <h1> Ayman Salah </h1>
+            <h1> Ayman Salah </h1>
+            <p>Add Merchant </p>
+            <p>,Add Category[ For merchant ]</p>
+            <p>, See Orders ,see users ,</p>
+            <p> See Billing</p>
         </div>
     </div>
-
 @endsection
 
 @section('scripts')
