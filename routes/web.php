1ffafbcda0e15e3a7ce11861adfec3c6788d2e1f
Ayman Salah <80077545+AymanSalah1994@users.noreply.github.com>
2022-09-11 22:03:43 +0200

    Code Refactor

--- routes/web.php
+++ routes/web.php
@@ -6,17 +6,6 @@ use  App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\Store\StoreController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', [StoreController::class , 'index'])->name('store.index');
 
 Auth::routes();
