e127ad49c9fd23c848d45d259b27c095b4dee828
Ayman Salah <80077545+AymanSalah1994@users.noreply.github.com>
2022-09-11 22:58:06 +0200

    b

--- app/Http/Controllers/Store/StoreController.php
+++ app/Http/Controllers/Store/StoreController.php
@@ -8,11 +8,11 @@ use Illuminate\Http\Request;
 
 class StoreController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         // TODO : Changing this Main Page
         // Make a New Folder Outside Layouts and Exntend the Main Page
-        
-        $featured_products = Product::where('trending' , '1')->take(3)->get() ; 
-        return view('store.home' , compact('featured_products')) ; 
+        $featured_products = Product::where('trending', '1')->take(3)->get();
+        return view('store.home', compact('featured_products'));
     }
 }
