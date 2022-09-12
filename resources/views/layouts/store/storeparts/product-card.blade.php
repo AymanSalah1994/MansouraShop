8e013cb42529dd3d5d86fd7bbadddd2d3fe26a1f
Ayman Salah <80077545+AymanSalah1994@users.noreply.github.com>
2022-09-12 04:56:49 +0200

    Product Details View Boilderblate

--- resources/views/layouts/store/storeparts/product-card.blade.php
+++ resources/views/layouts/store/storeparts/product-card.blade.php
@@ -2,7 +2,9 @@
     <div class="card">
         <img src="{{ Storage::url($product->product_picture) }}" alt="product-image">
         <div class="card-body">
-            <h5>{{ $product->name }}</h5>
+            <a href="{{ route('product.details' ,$product->id)}}">
+                <h5>{{ $product->name }}</h5>
+            </a>
             <span class="float-start">{{ $product->selling_price }}</span>
             <span class="float-end"><s>{{ $product->original_price }}</s></span>
         </div>
